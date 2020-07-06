d16d0721a5b29a9547665954fd40ffbf4b710a97
Doni <doniattokurovich@gmail.com>
2020-07-06 19:47:54 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+let url = 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=LKri6hXvoQfyOCqLABku6i9fOgIokbfC&limit=5';
+
+let container = document.querySelector('.container');
+let form = document.querySelector('.form');
+let input = document.querySelector('.input');
+let submit = document.querySelector('.submit-btn');
+let gifs = document.querySelector('.gifs');
+
+form.addEventListener('click', function (e) {
+  e.preventDefault();
+  let target = e.target;
+  let inputText = input.value;
+  url = 'http://api.giphy.com/v1/gifs/search?q=' + inputText.split(' ').join('+') + '&api_key=LKri6hXvoQfyOCqLABku6i9fOgIokbfC&limit=5';
+  if (target.type == 'submit') {
+
+    fetch(url)
+      .then(data => data.json())
+      .catch(error => console.error(error))
+      .then(data => {
+
+        data.data.forEach(item => {
+          if (gifs.children.length < 6) {
+            let img = document.createElement('img');
+            img.src = item.images.original.url
+            gifs.appendChild(img);
+          } else {
+            gifs.innerHTML = "";
+          }
+
+        });
+
+      });
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+});
